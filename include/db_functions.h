b2c0c1e68fca95a3d8a1d6650586f5ef12d6db8b
antonmyr <antonmyrbergg@gmail.com>
2020-09-21 22:50:25 +0200

    Added fcntl and formatting

--- include/db_functions.h
+++ include/db_functions.h
@@ -1,17 +1,18 @@
 #ifndef DB_FUNCTIONS_H
 #define DB_FUNCTIONS_H
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdbool.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
-#include "table_t.h"
-#include "request.h"
 #include "queue.h"
+#include "request.h"
+#include "table_t.h"
 
 #define META_FILE "meta.txt"
 #define COL_DELIM ","
@@ -19,20 +20,19 @@
 #define ROW_DELIM "\n"
 
 typedef struct return_value return_value;
-struct return_value
-{
-	char* msg;
-	bool success;
+struct return_value {
+  char *msg;
+  bool success;
 };
 
-void execute_request(void* arg);
+void execute_request(void *arg);
 
-void create_table(request_t* req, return_value* ret_val);
-void print_tables(return_value* ret_val);
-void print_schema(char* name, return_value* ret_val);
-void add_table(table_t* table);
-bool table_exists(char* name);
+void create_table(request_t *req, return_value *ret_val);
+void print_tables(return_value *ret_val);
+void print_schema(char *name, return_value *ret_val);
+void add_table(table_t *table);
+bool table_exists(char *name);
 
-bool is_valid_varchar(column_t* col);
+bool is_valid_varchar(column_t *col);
 
 #endif
