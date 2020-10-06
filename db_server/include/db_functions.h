90294d0e37f061f6508bda5dd97bd4e863bfc209
Anton <anton.lindstrand0@gmail.com>
2020-10-06 12:55:55 +0200

    Fixed clang-format
    added function quit_connection() that quits a connection, this should have been done when the .quit request was implemented but i forgot

--- db_server/include/db_functions.h
+++ db_server/include/db_functions.h
@@ -37,8 +37,8 @@
 
 typedef struct return_value return_value;
 struct return_value {
-    char *msg;
-    bool success;
+	char *msg;
+	bool success;
 };
 
 void execute_request(void *arg);
@@ -48,8 +48,9 @@ void print_tables(return_value *ret_val);
 void print_schema(char *name, return_value *ret_val);
 void add_table(table_t *table, FILE *meta, char *log_file);
 void select_table(char *name, client_request *cli_req);
-void drop_table(client_request *cli_req /*char *name*/, return_value *ret_val);
+void drop_table(client_request *cli_req, return_value *ret_val);
 bool table_exists(char *name, FILE *meta);
+void quit_connection(client_request *cli_req);
 int create_data_file(char *name);
 void insert_data(request_t *req, return_value *ret_val);
 void create_template_column(char *name, FILE *meta, column_t **first, int *chars_in_row);
@@ -59,7 +60,7 @@ void log_to_file(char *file_name, const char *format, ...);
 bool is_valid_varchar(column_t *col);
 
 int column_to_buffer(column_t *table_column, column_t *input_column,
-                     dynamicstr *output_buffer, char **ret_msg);
+					 dynamicstr *output_buffer, char **ret_msg);
 int populate_column(column_t *current, char *table_row);
 int unpopulate_column(column_t *current);
 
