0d673f202435c4e97e9611e71b95eb88c9f634fa
antonmyr <antonmyrbergg@gmail.com>
2020-10-09 14:57:47 +0200

    Added alternative struct to column_t.

--- db_server/include/db_functions.h
+++ db_server/include/db_functions.h
@@ -41,7 +41,26 @@ typedef struct is_primary_key is_primary_key;
 struct is_primary_key {
 	int total_row_size;
 	int size_to_pk;
+	char *name;
 	bool found;
+};
+
+typedef struct table_column_t table_column_t;
+struct table_column_t {
+	/* name of the column */
+	char *name;
+	/* indicates the columns data type */
+	char data_type;
+	/* indicates if column is the PRIMARY KEY column */
+	char is_primary_key;
+	/* INT value for INSERT or UPDATE statement */
+	char *char_val;
+	/* Offset from the start of the row to this column*/
+	int offset;
+	/* total size of this column */
+	int total_size;
+	/* pointer to next column entry */
+	table_column_t *next;
 };
 
 void execute_request(void *arg);
@@ -56,15 +75,16 @@ bool table_exists(char *name, FILE *meta);
 void quit_connection(client_request *cli_req);
 int create_data_file(char *name);
 void insert_data(client_request *cli_req, char **client_msg);
-void create_template_column(char *name, FILE *meta, column_t **first, int *chars_in_row);
+void create_template_column(char *name, FILE *meta, table_column_t **first, int *chars_in_row);
 int create_full_data_path_from_name(char *name, char **full_path);
 void log_to_file(const char *format, ...);
+void update_row(client_request *cli_req, char **client_msg);
 
 bool is_valid_varchar(column_t *col);
 
-int column_to_buffer(column_t *table_column, column_t *input_column,
+int column_to_buffer(table_column_t *table_column, column_t *input_column,
 					 dynamicstr *output_buffer, int primary_key, char **client_msg);
-int populate_column(column_t *current, char *table_row, is_primary_key *is_pk);
-int unpopulate_column(column_t *current);
+int populate_column(table_column_t *current, char *table_row, is_primary_key *is_pk);
+int unpopulate_column(table_column_t *current);
 
 #endif
