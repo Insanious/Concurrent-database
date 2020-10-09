32897b852764aa9fc9530c613795a26d65144b8f
antonmyr <antonmyrbergg@gmail.com>
2020-10-09 14:58:19 +0200

    Added another field

--- db_server/include/table_t.h
+++ db_server/include/table_t.h
@@ -4,12 +4,12 @@
 #include "request.h"
 
 typedef struct table_t table_t;
-
 struct table_t {
 	/* name of the table */
-	char* name;
+	char *name;
 	/* the columns in the table */
-	column_t* columns;
+	column_t *columns;
+	column_t *where;
 };
 
 #endif
